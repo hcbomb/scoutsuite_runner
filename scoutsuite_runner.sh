cead277816a285042815620c21a594a1b00c5823
hcanivel <hcanivel@splunk.com>
2020-07-16 05:29:53 -0400

    added scoutsuite conversion to main runner script removed extraneous while loop from main runner script updated extraction of special chars from aws profile name (normalize) initiated timestamp recogniztion from scoutsuite results file (versus dynamic)

--- scoutsuite_runner.sh
+++ scoutsuite_runner.sh
@@ -6,9 +6,11 @@ LOGDIR=$RUNNER_DIR/log
 
 SCOUTSUITE_SCRIPT=$SCOUTSUITE/ScoutSuite/scout.py
 GET_ORG_SCRIPT=$RUNNER_DIR/get_org_list.py
+SS_CONVERTER_SCRIPT=$RUNNER_DIR/ss_converter_aws.py
 PROFILE=$RUNNER_DIR/aws_profile_list.txt
 PROFILE_BUILDER_SCRIPT=$RUNNER_DIR/aws_configurate.sh
-LOGFILE=$LOGDIR/collector.botorator.log
+LOGFILE=$LOGDIR/collector.scoutsuite_runner.log
+RABBITFILE=$RUNNER_DIR/rabbit.txt # used to easily track newly generated Scoutsuite report files
 
 ORIG_AWS_PROFILE="Voltron_DCN"
 TIMESTAMP=`date +"%Y-%m-%d %H:%M:%S.%3N%z"`
@@ -18,10 +20,11 @@ DATESTAMP_TAG=`date +"%Y-%m-%d"`
 MAX_NPROC=10
 NUM=0
 TOTAL=0
+CNUM=0
 
 # Throttling params
-MAX_WORKERS=2
-MAX_RATE=10 # describe/list API calls per second
+MAX_WORKERS=5
+MAX_RATE=100 # describe/list API calls per second
 
 function queue {
     QUEUE="$QUEUE $1"
@@ -59,54 +62,66 @@ if [ ! -d $REPODIR ]; then
     mkdir -p $REPORT_DIR/$DATESTAMP_TAG
 fi
 
+# release the rabbit
+touch $RABBITFILE
+
 source $SCOUTSUITE/venv/bin/activate
 echo "$TIMESTAMP creating aws account list from org..." >> $LOGFILE
 ts1=`date +%s`
 echo "executing: python3 $GET_ORG_SCRIPT -p $ORIG_AWS_PROFILE" >> $LOGFILE
-#python3 $GET_ORG_SCRIPT -p $ORIG_AWS_PROFILE >> $LOGFILE
+python3 $GET_ORG_SCRIPT -p $ORIG_AWS_PROFILE >> $LOGFILE
 te1=`date +%s`
 duration=$((te1 - ts1))
 
 echo "$TIMESTAMP aws account list created, proceeding to build aws config. elapsed: $(($duration / 60)) min and $(($duration % 60)) sec" >> $LOGFILE
 
 ts1=`date +%s`
-#bash $PROFILE_BUILDER_SCRIPT >> $LOGFILE
+bash $PROFILE_BUILDER_SCRIPT >> $LOGFILE
 te1=`date +%s`
 duration=$((te1 - ts1))
 
 echo "$TIMESTAMP aws config, proceeding to run botorator scoutsuite rate limiter. elapsed: $(($duration / 60)) min and $(($duration % 60)) sec" >> $LOGFILE
 
 ts2=`date +%s`
-PROFILE=$SCOUTSUITE/test_it.aws.profile.txt
+#PROFILE=$RUNNER_DIR/test_it.aws.profile.txt
+echo "profile path: $PROFILE"
 for AWS_PROFILE in `cat $PROFILE`; do
     echo "$TIMESTAMP processing AWS_PROFILE=$AWS_PROFILE" >> $LOGFILE
 
-    # send error output to logfile
-    while read i
-    do
-        TOTAL=$((TOTAL+1))
-        echo "$TIMESTAMP outputting to filename=$LOGDIR/$DATESTAMP_TAG/scoutsuite.$AWS_PROFILE.$TIMESTAMP_TAG.log aws profile: $AWS_PROFILE count: $TOTAL" >> $LOGFILE
-		echo "python3 $SCOUTSUITE_SCRIPT aws --profile $AWS_PROFILE --report-dir $REPORT_DIR/$DATESTAMP_TAG/$AWS_PROFILE.$TIMESTAMP_TAG --report-name $AWS_PROFILE " >> $LOGDIR/$DATESTAMP_TAG/scoutsuite.$AWS_PROFILE.$TIMESTAMP_TAG.log 2>&1 
-		exit
-        python3 $SCOUTSUITE_SCRIPT aws --profile $AWS_PROFILE --max-workers $MAX_WORKERS --max-rate $MAX_RATE --report-dir $REPORT_DIR/$DATESTAMP_TAG/$AWS_PROFILE.$TIMESTAMP_TAG --report-name $AWS_PROFILE >> $LOGDIR/$DATESTAMP_TAG/scoutsuite.$AWS_PROFILE.$TIMESTAMP_TAG.log 2>&1
-        #python3 $SCOUTSUITE_SCRIPT aws --profile $AWS_PROFILE --report-dir $REPORT_DIR/$DATESTAMP_TAG/$AWS_PROFILE.$TIMESTAMP_TAG --report-name $AWS_PROFILE >> $LOGDIR/$DATESTAMP_TAG/scoutsuite.$AWS_PROFILE.$TIMESTAMP_TAG.log 2>&1
-        PID=$!
+    TOTAL=$((TOTAL+1))
+# print executed scoutsuite run into debug log
+    echo "python3 $SCOUTSUITE_SCRIPT aws --profile $AWS_PROFILE --max-workers $MAX_WORKERS --max-rate $MAX_RATE --report-dir $REPORT_DIR/$DATESTAMP_TAG/$AWS_PROFILE.$TIMESTAMP_TAG --report-name $AWS_PROFILE" >> $LOGDIR/$DATESTAMP_TAG/scoutsuite.$AWS_PROFILE.$TIMESTAMP_TAG.log 2>&1
+
+#    python3 $SCOUTSUITE_SCRIPT aws --profile "$AWS_PROFILE" --max-workers $MAX_WORKERS --max-rate $MAX_RATE --report-dir "$REPORT_DIR/$DATESTAMP_TAG/$AWS_PROFILE.$TIMESTAMP_TAG" --report-name "$AWS_PROFILE" -f >> "$LOGDIR/$DATESTAMP_TAG/scoutsuite.$AWS_PROFILE.$TIMESTAMP_TAG.log" 2>&1 &
+# no rate limiting
+    #python3 $SCOUTSUITE_SCRIPT aws --profile $AWS_PROFILE --report-dir $REPORT_DIR/$DATESTAMP_TAG/$AWS_PROFILE.$TIMESTAMP_TAG --report-name $AWS_PROFILE >> $LOGDIR/$DATESTAMP_TAG/scoutsuite.$AWS_PROFILE.$TIMESTAMP_TAG.log 2>&1
+    PID=$!
+    if [[ ! -z "$PID" ]]; then
+        echo "$TIMESTAMP outputting to filename=$LOGDIR/$DATESTAMP_TAG/scoutsuite.$AWS_PROFILE.$TIMESTAMP_TAG.log aws profile: $AWS_PROFILE count: $TOTAL pid: $PID" >> $LOGFILE
         queue $PID
 
         while [ $NUM -ge $MAX_NPROC ]; do
             checkqueue
             sleep 0.01
         done
+    fi
 
-    te2=`date +%s`
-    duration=$((te2 - ts2))
-    duration2=$((te2 - ts1))
     echo "$TIMESTAMP processing scanning AWS_PROFILE=$AWS_PROFILE" >> $LOGFILE
-    echo -e "$TIMESTAMP completed AWS_PROFILE=$AWS_PROFILE. elapsed ( only): $(($duration / 60))  min and $(($duration % 60)) sec\t$(($duration2 / 60))  min and $(($duration2 % 60)) sec" >> $LOGFILE
 
-    done < $PROFILE
 done
 
-echo "$TIMESTAMP ScoutScout rate limiter job complete." >> $LOGFILE
+te2=`date +%s`
+duration=$((te2 - ts2))
+duration2=$((te2 - ts1))
+echo -e "$TIMESTAMP ScoutSuite runner job complete. total time elapsed: $(($duration / 60))  min and $(($duration % 60)) sec\t$(($duration2 / 60))  min and $(($duration2 % 60)) sec" >> $LOGFILE
 
+# convert all newly generated ScoutSuite report files into Splunk-friendly data events
+for REPORT in `find $REPORT_DIR -cnewer $RABBITFILE -type f -name 'scoutsuite_results_*.js'`; do
+    CNUM=$((CNUM+1))
+    ORIG_REPORT_FOLDER=`dirname $REPORT`
+    echo "$TIMESTAMP converting $REPORT >> $ORIG_REPORT_FOLDER/report.scoutsuite.$AWS_PROFILE.txt" >> $LOGFILE
+    python3 $SS_CONVERTER_SCRIPT -s "$REPORT" -d "$ORIG_REPORT_FOLDER/report.scoutsuite.$AWS_PROFILE.txt"
+done
+
+echo "$TIMESTAMP successfully converted $CNUM ScoutSuite reports" >> $LOGFILE
 
